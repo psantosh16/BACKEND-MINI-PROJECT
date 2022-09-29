9108db35bd828da933c5d62bdbe29e158b1aa570
Santosh Phadtare <99491679+psantosh16@users.noreply.github.com>
2022-09-29 12:12:20 +0530

    Added Files Successfully
    Don't make any unnecessary changes in repo.

---
+++ dataBase/database.js
@@ -0,0 +1,29 @@
+// RULES ==>
+// [] array
+// {} objects
+// {id: , name: "" , price: "" , imgUrl: "" },  <===== this
+//____________________________________________________________________
+//
+// Note: (1) Comma's between every objects and values should be between double inverted commas
+//       (2) After every object ther should be an comma after {} , <= see this
+//       (3) Every object must have unique id and object properties are caseSensitive
+// Thanks for reading :)
+
+// Actual DataBase ==>
+
+const dataBase = [
+  {
+    id: 0,
+    name: "Dal fry",
+    price: "₹ 300",
+    imgUrl: "../Assets/img0.jpeg",
+  },
+  {
+    id: 1,
+    name: "Chicken Handi",
+    price: "₹ 450",
+    imgUrl: "../Assets/img1.jpeg",
+  },
+];
+
+export default dataBase;
