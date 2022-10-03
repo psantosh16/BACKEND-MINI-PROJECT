6da0db68c05baa82e45d9e62dfa8a19997a13d60
Pradeep Rathod <113924351+pradeepR22@users.noreply.github.com>
2022-10-03 14:06:10 +0530

    Update database.js

--- dataBase/database.js
+++ dataBase/database.js
@@ -12,19 +12,59 @@
 // Actual DataBase ==>
 
 const dataBase = [
+  
+  // Non-veg
   {
     id: 0,
     name: "Octopus Magetta",
     price: "₹ 300",
+    veg:"false",
     imgUrl: "https://images.unsplash.com/photo-1559070135-f259b369bf87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
 // imgUrl: "https://imgurl...."  => best option
   },
   {
-    id: 1,
+    id: 2,
     name: "Chicken kofta",
     price: "₹ 450",
+    veg:"false",
     imgUrl: "https://recipe52.com/wp-content/uploads/2022/03/chicken-handi-10.jpg",
   },
+  
+  
+  
+  
+  
+// Veg   
+   {
+    id: 3,
+    name: "Pav Bhaji",
+    price: "₹ 120",
+    veg:"true",
+    imgUrl: "https://media.istockphoto.com/photos/masala-pavbhaji-or-pav-bhaji-picture-id800368868?b=1&k=20&m=800368868&s=612x612&w=0&h=DfmVXU81F6JmL15aI8i1JM6R-EOI4G3-JoZp1bYfk10="
+   },
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 ];
 
 export default dataBase;
